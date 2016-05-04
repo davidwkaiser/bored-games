2b94f8870d9e276f1965b26cae2722a9a7a8bd2e
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2016-05-04 17:45:34 -0500

    move everything around

--- boredgames/app/models/game_mechanic.rb
+++
@@ -1,5 +0,0 @@
-class GameMechanic < ActiveRecord::Base
-  belongs_to :game
-  belongs_to :mechanic
-
-end
--- boredgames/app/models/mechanic.rb
+++
@@ -1,5 +0,0 @@
-class Mechanic < ActiveRecord::Base
-  validates :description, presence: true
-  has_many :game_mechanics
-  has_many :games, through: :game_mechanics
-end
