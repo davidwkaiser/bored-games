665248e47e6dc18bb84ee3cf92a9a35e2d713030
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2016-05-04 17:16:41 -0500

    debugged

--- boredgames/db/seeds.rb
+++ boredgames/db/seeds.rb
@@ -12,7 +12,7 @@ results = []
 5.times do
 	page += 1
 	doc = Nokogiri::XML(open("http://boardgamegeek.com/browse/boardgame/page/#{page}"))
-	parsed_data = doc.css("a").map { |link| link['href'] }.select{ |path| path =~ /\A\/boardgame/ }.map { |url| url.split("/") }.map { |element| element[2] }.uniq
+	parsed_data = doc.css("a").map { |link| link['href'] }.select{ |path| path =~ /\A\/boardgame\// }.map { |url| url.split("/") }.map { |element| element[2] }.uniq
  	parsed_data.delete_if { |id| id == "random"}
  	results << parsed_data
 end
@@ -26,24 +26,32 @@ end
 
 results.each do |stuff|
 	game_data = parser(stuff)
-	title = game_data.css('name')[0][:value]
-	description = game_data.css("description").text.gsub(/&#10;/, "")
-	play_time = game_data.css('playingtime')[0][:value]
-	min_players = game_data.css('minplayers')[0][:value]
-	max_players = game_data.css('maxplayers')[0][:value]
-	published = game_data.css('yearpublished')[0][:value]
-	mechanics = game_data.css("link").select { |link| link[:type] == "boardgamemechanic" }.map { |link| link[:value] }
+	if game_data != nil
+		title = game_data.css('name')[0][:value]
+		description = game_data.css("description").text.gsub(/&#10;/, "")
+		play_time = game_data.css('playingtime')[0][:value]
+		min_players = game_data.css('minplayers')[0][:value]
+		max_players = game_data.css('maxplayers')[0][:value]
+		published = game_data.css('yearpublished')[0][:value]
+		image_link = game_data.css('image').text
+		puts image_link
+		mechanics = game_data.css("link").select { |link| link[:type] == "boardgamemechanic" }.map { |link| link[:value] }
 
-	game = Game.create({name: title,
-											description: description,
-											play_time: play_time,
-											min_players: min_players,
-											max_players: max_players,
-											year_published: published})
+		game = Game.create({name: title,
+												description: description,
+												play_time: play_time,
+												image: image_link,
+												min_players: min_players,
+												max_players: max_players,
+												year_published: published})
 
-	mechanics.each do |mechanic|
-		game.mechanics.find_or_create_by(description: mechanic)
+		mechanics.each do |mechanic|
+			game.mechanics.find_or_create_by(description: mechanic)
+		end
+		puts "all good!"
+	else
+		puts "There was bug in the system"
 	end
 
-	sleep 1
+	sleep 0.5
 end
