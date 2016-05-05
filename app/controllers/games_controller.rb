9453acc4a332cf48921ff08cd3a7ba59fe3009c2
Dave Kaiser <email": null>
2016-05-05 17:30:44 -0500

    add sort by popularity

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -13,9 +13,16 @@ class GamesController < ApplicationController
     @games = Game.paginate(:page => params[:page], :per_page => 10).order('name ASC')
   end
 
+  # def popularity
+  #   @games = Game.paginate(:page => params[:page], :per_page => 10).order(self.votes.count)
+  # end
+
   def popularity
-    @games = Game.paginate(:page => params[:page], :per_page => 10).order(votes.count)
+    @games = Game.joins(:votes).group("games.id").order("count(games.id) DESC")
+    @games = @games.paginate(:page => params[:page], :per_page => 10)
+
   end
+
 
 
 
