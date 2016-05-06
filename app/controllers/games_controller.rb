3f6a51f1acf41be25bc81e457bef37f1db2c9fc9
Joshua Lugo <email": null>
2016-05-06 14:43:22 -0500

    Filter by popular mechanics minimally functional

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,8 @@
 class GamesController < ApplicationController
-
   def index
     # @games = Game.all
     @games = Game.paginate(:page => params[:page], :per_page => 10)
+    @mechanics = Mechanic.top_mechanics
   end
 
   def show
@@ -11,19 +11,32 @@ class GamesController < ApplicationController
 
   def names
     @games = Game.paginate(:page => params[:page], :per_page => 10).order('name ASC')
+    @mechanics = Mechanic.top_mechanics
   end
-
-  # def popularity
-  #   @games = Game.paginate(:page => params[:page], :per_page => 10).order(self.votes.count)
-  # end
 
   def popularity
     @games = Game.joins(:votes).group("games.id").order("count(games.id) DESC")
     @games = @games.paginate(:page => params[:page], :per_page => 10)
-
+    @mechanics = Mechanic.top_mechanics
   end
 
+  def mechanics
+    @games = []
+    params[:stuff].each do |id, value|
+      if value.to_i > 0
+        mechanic = Mechanic.find(id.to_i)
+        @games << mechanic.games
+      end
+    end
+    # @games = Game.joins(:mechanics).group("mechanic.id").order('name ASC')
+    @games.uniq!
+    # @games = @games.paginate(:page => params[:page], :per_page => 10)
+    @mechanics = Mechanic.top_mechanics
+  end
 
+  private
 
+  def mech_params
 
+  end
 end
